ec76012d400a8cd230507e89b3a7e03a33af4867
Antonio Goncalves <antonio.goncalves@gmail.com>
2024-01-17 07:14:51 +0100

    Chatting

---
+++ chatting/src/main/java/org/agoncal/fascicle/langchain4j/chatting/Tokenization.java
@@ -0,0 +1,17 @@
+package org.agoncal.fascicle.langchain4j.chatting;
+
+public class Tokenization {
+
+  // tag::adocSnippet[]
+  String prompt = """
+    Isaac Asimov (1920-1992), a Russian-born American author and biochemist,
+    achieved renown as a prolific science fiction writer and science popularizer.
+    Immigrating to the U.S. as a child, he began his literary career with the
+    publication of "Marooned off Vesta" in 1939. Asimov's extensive body of work
+    includes iconic novels like "Foundation" and "I, Robot." Beyond fiction,
+    he became a revered science communicator, writing numerous accessible books
+    and essays that demystified complex scientific concepts for a broad audience,
+    leaving an enduring legacy in both literature and science education.
+    """;
+  // end::adocSnippet[]
+}
