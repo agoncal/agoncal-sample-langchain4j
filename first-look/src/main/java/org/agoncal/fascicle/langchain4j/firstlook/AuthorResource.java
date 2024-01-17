54c101a82990da0bca03fdfb4c69d61ce3521454
Antonio Goncalves <antonio.goncalves@gmail.com>
2024-01-17 16:25:37 +0100

    Simplify

--- first-look/src/main/java/org/agoncal/fascicle/langchain4j/firstlook/AuthorResource.java
+++ first-look/src/main/java/org/agoncal/fascicle/langchain4j/firstlook/AuthorResource.java
@@ -1,16 +1,13 @@
 package org.agoncal.fascicle.langchain4j.firstlook;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
-import static java.time.Duration.ofSeconds;
-
-// tag::adocSnippet[]
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import static dev.langchain4j.model.openai.OpenAiModelName.GPT_3_5_TURBO;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import static java.time.Duration.ofSeconds;
 
 // tag::adocSkip[]
 
@@ -28,7 +25,7 @@ public class AuthorResource {
 
   private static final String OPENAI_API_KEY = System.getenv("OPENAI_API_KEY");
 
-  private String[] scifiAuthors = {"Isaac Asimov", "Nora Jemisin", "Douglas Adams"};
+//  private String[] scifiAuthors = {"Isaac Asimov", "Nora Jemisin", "Douglas Adams"};
 
   @GET
   public String getIsaacAsimovBiography() {
@@ -49,24 +46,24 @@ public class AuthorResource {
     return biography;
   }
 
-  @GET
-  @Path("/{index}")
-  public String getAuthorBiography(@PathParam("index") int index) {
-
-    ChatLanguageModel model = OpenAiChatModel.builder()
-      .apiKey(OPENAI_API_KEY)
-      .modelName(GPT_3_5_TURBO)
-      .temperature(0.3)
-      .timeout(ofSeconds(60))
-      .logRequests(true)
-      .logResponses(true)
-      .build();
-
-    String prompt = "Write a short biography about " + scifiAuthors[index];
-
-    String biography = model.generate(prompt);
-
-    return biography;
-  }
+//  @GET
+//  @Path("/{index}")
+//  public String getAuthorBiography(@PathParam("index") int index) {
+//
+//    ChatLanguageModel model = OpenAiChatModel.builder()
+//      .apiKey(OPENAI_API_KEY)
+//      .modelName(GPT_3_5_TURBO)
+//      .temperature(0.3)
+//      .timeout(ofSeconds(60))
+//      .logRequests(true)
+//      .logResponses(true)
+//      .build();
+//
+//    String prompt = "Write a short biography about " + scifiAuthors[index];
+//
+//    String biography = model.generate(prompt);
+//
+//    return biography;
+//  }
 }
 // end::adocSnippet[]
