ba6407bd53473e3be1e8be771859daa667cc9723
Antonio Goncalves <antonio.goncalves@gmail.com>
2024-01-23 21:22:44 +0100

    Wiremock

--- wiremock.sh
+++ wiremock.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+# Execute Wiremock
+# http://localhost:8080/__admin/recorder
+# http://localhost:8080/__admin/mappings
+# http://localhost:8080/__admin/requests
+
 java -jar ~/.m2/repository/org/wiremock/wiremock-standalone/3.3.1/wiremock-standalone-3.3.1.jar
