a4ec3b885691c8e71a09eeeb72d0ddf632c10b86
Antonio Goncalves <antonio.goncalves@gmail.com>
2024-01-25 06:17:25 +0100

    Chat APIs

---
+++ chatting/src/main/java/org/agoncal/fascicle/langchain4j/chatting/MusicianService.java
@@ -0,0 +1,124 @@
+package org.agoncal.fascicle.langchain4j.chatting;
+
+// tag::adocSnippet[]
+
+import dev.langchain4j.model.input.Prompt;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.model.openai.OpenAiLanguageModel;
+import static dev.langchain4j.model.openai.OpenAiModelName.GPT_3_5_TURBO;
+import dev.langchain4j.model.output.FinishReason;
+import dev.langchain4j.model.output.Response;
+import dev.langchain4j.model.output.TokenUsage;
+
+// tag::adocSkip[]
+
+/**
+ * @author Antonio Goncalves
+ * http://www.antoniogoncalves.org
+ * --
+ */
+// end::adocSkip[]
+public class MusicianService {
+
+  public static void main(String[] args) {
+    MusicianService musicianService = new MusicianService();
+
+    musicianService.useOpenAiLanguageModel();
+    musicianService.useOpenAiLanguageModelPrompt();
+    musicianService.useOpenAiLanguageModelBuilder();
+
+    musicianService.useOpenAiChatModel();
+    musicianService.useOpenAiChatModelBuilder();
+  }
+
+  private static final String OPENAI_API_KEY = System.getenv("OPENAI_API_KEY");
+
+  private static final String PROMPT = "When was the first Beatles album released?";
+
+  // #############################
+  // ### OPENAI LANGUAGE MODEL ###
+  // #############################
+  public void useOpenAiLanguageModel() {
+    System.out.println("### useOpenAiLanguageModel");
+
+    OpenAiLanguageModel model = OpenAiLanguageModel.withApiKey(OPENAI_API_KEY);
+
+    Response<String> completion = model.generate("When was the first Beatles album released?");
+
+    String content = completion.content();
+    FinishReason finishReason = completion.finishReason();
+    TokenUsage tokenUsage = completion.tokenUsage();
+
+    System.out.println(content);
+    System.out.println(finishReason.name());
+    System.out.println(tokenUsage.inputTokenCount());
+    System.out.println(tokenUsage.outputTokenCount());
+    System.out.println(tokenUsage.totalTokenCount());
+  }
+
+  public void useOpenAiLanguageModelPrompt() {
+    System.out.println("### useOpenAiLanguageModelPrompt");
+
+    OpenAiLanguageModel model = OpenAiLanguageModel.withApiKey(OPENAI_API_KEY);
+
+    Prompt prompt = new Prompt("When was the first Beatles album released?");
+    Response<String> completion = model.generate(prompt);
+
+    String content = completion.content();
+    FinishReason finishReason = completion.finishReason();
+    TokenUsage tokenUsage = completion.tokenUsage();
+
+    System.out.println(content);
+    System.out.println(finishReason.name());
+    System.out.println(tokenUsage.inputTokenCount());
+    System.out.println(tokenUsage.outputTokenCount());
+    System.out.println(tokenUsage.totalTokenCount());
+  }
+
+  public void useOpenAiLanguageModelBuilder() {
+    System.out.println("### useOpenAiLanguageModelBuilder");
+
+    OpenAiLanguageModel model = OpenAiLanguageModel.builder()
+      .apiKey(OPENAI_API_KEY)
+      .temperature(0.3)
+      .logRequests(true)
+      .logResponses(true)
+      .build();
+
+    Response<String> completion = model.generate(PROMPT);
+
+    System.out.println(completion.content());
+    System.out.println(completion.finishReason());
+    System.out.println(completion.tokenUsage());
+  }
+
+  // #########################
+  // ### OPENAI CHAT MODEL ###
+  // #########################
+  public void useOpenAiChatModel() {
+    System.out.println("### useOpenAiChatModel");
+
+    OpenAiChatModel model = OpenAiChatModel.withApiKey(OPENAI_API_KEY);
+
+    String completion = model.generate("When was the first Rolling Stones album released?");
+
+    System.out.println(completion);
+  }
+
+  public void useOpenAiChatModelBuilder() {
+    System.out.println("### useOpenAiChatModelBuilder");
+
+    OpenAiChatModel model = OpenAiChatModel.builder()
+      .modelName(GPT_3_5_TURBO)
+      .apiKey(OPENAI_API_KEY)
+      .temperature(0.9)
+      .logRequests(true)
+      .logResponses(true)
+      .build();
+
+    String completion = model.generate("When was the first Rolling Stones album released?");
+
+    System.out.println(completion);
+  }
+}
+// end::adocSnippet[]
