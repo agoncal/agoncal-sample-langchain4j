8a5b077e2c6c6de5715507e673907f7713949c1f
Antonio Goncalves <antonio.goncalves@gmail.com>
2024-01-26 07:08:23 +0100

    Tokens

--- chatting/src/main/java/org/agoncal/fascicle/langchain4j/chatting/Tokenization.java
+++ chatting/src/main/java/org/agoncal/fascicle/langchain4j/chatting/Tokenization.java
@@ -1,5 +1,10 @@
 package org.agoncal.fascicle.langchain4j.chatting;
 
+import static dev.langchain4j.model.openai.OpenAiModelName.GPT_3_5_TURBO_0301;
+import dev.langchain4j.model.openai.OpenAiTokenizer;
+
+import java.util.List;
+
 public class Tokenization {
 
   // tag::adocSnippet[]
@@ -14,4 +19,27 @@ public class Tokenization {
     leaving an enduring legacy in both literature and science education.
     """;
   // end::adocSnippet[]
+
+  public static void main(String[] args) {
+    Tokenization tokenization = new Tokenization();
+    tokenization.tokenizeOpenAI();
+  }
+
+  private void tokenizeOpenAI() {
+    System.out.println("### tokenizeOpenAI");
+    OpenAiTokenizer tokenizer = new OpenAiTokenizer(GPT_3_5_TURBO_0301);
+
+    // Estimate
+    int nbOfTokens = tokenizer.estimateTokenCountInText(prompt);
+    System.out.println("Number of tokens: " + nbOfTokens + "\n");
+
+    // Encode
+    List<Integer> tokens = tokenizer.encode(prompt);
+    tokens.forEach(token -> System.out.print(token + " "));
+    System.out.println("\n");
+
+    // Decode
+    String prompt = tokenizer.decode(tokens);
+    System.out.println("Prompt: " + prompt);
+  }
 }
