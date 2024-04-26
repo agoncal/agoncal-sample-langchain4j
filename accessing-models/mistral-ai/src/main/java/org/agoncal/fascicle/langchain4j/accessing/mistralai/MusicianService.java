18de55e3fa7717d57035ec64e9e86d11115dd5b4
Antonio Goncalves <antonio.goncalves@gmail.com>
2024-04-26 13:35:51 +0200

    Mistral

--- accessing-models/mistral-ai/src/main/java/org/agoncal/fascicle/langchain4j/accessing/mistralai/MusicianService.java
+++ accessing-models/mistral-ai/src/main/java/org/agoncal/fascicle/langchain4j/accessing/mistralai/MusicianService.java
@@ -18,21 +18,17 @@ public class MusicianService {
     musicianService.useMistralAiChatModel();
   }
 
-  private static final String AZURE_OPENAI_KEY = System.getenv("AZURE_OPENAI_KEY");
-  private static final String AZURE_OPENAI_ENDPOINT = System.getenv("AZURE_OPENAI_ENDPOINT");
-  private static final String AZURE_OPENAI_DEPLOYMENT_NAME = System.getenv("AZURE_OPENAI_DEPLOYMENT_NAME");
+  private static final String MISTRAL_AI_API_KEY = System.getenv("MISTRAL_AI_API_KEY");
 
-  private static final String PROMPT = "When was the first Beatles album released?";
-
-  // ###############################
-  // ### AZURE OPENAI CHAT MODEL ###
-  // ###############################
+  // ##########################
+  // ### MISTRAL CHAT MODEL ###
+  // ##########################
   public void useMistralAiChatModel() {
     System.out.println("### useMistralAiChatModel");
 
     // tag::adocSnippet[]
     MistralAiChatModel model = MistralAiChatModel.builder()
-      .apiKey(AZURE_OPENAI_KEY)
+      .apiKey(MISTRAL_AI_API_KEY)
       .temperature(0.3)
       .build();
     // end::adocSnippet[]
