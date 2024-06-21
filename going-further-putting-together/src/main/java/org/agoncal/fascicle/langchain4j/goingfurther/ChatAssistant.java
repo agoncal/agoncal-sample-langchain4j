9772853b44f115dbda467f52c6985402084649a1
Antonio Goncalves <antonio.goncalves@gmail.com>
2024-06-21 09:17:33 +0200

    format code

--- going-further-putting-together/src/main/java/org/agoncal/fascicle/langchain4j/goingfurther/ChatAssistant.java
+++ going-further-putting-together/src/main/java/org/agoncal/fascicle/langchain4j/goingfurther/ChatAssistant.java
@@ -8,13 +8,13 @@ import dev.langchain4j.service.V;
 // tag::adocSnippet[]
 public interface ChatAssistant {
 
-    @SystemMessage("""
-            You are an expert of the company Vintage Store.
-            Focus on the customer and answer the questions.
-            Keep your answers short and to the point.
-            """)
-    @UserMessage("Here is my question: {{question}}")
-    String chat(@V("question") String question);
+  @SystemMessage("""
+    You are an expert of the company Vintage Store.
+    Focus on the customer and answer the questions.
+    Keep your answers short and to the point.
+    """)
+  @UserMessage("Here is my question: {{question}}")
+  String chat(@V("question") String question);
 
 }
 // end::adocSnippet[]
