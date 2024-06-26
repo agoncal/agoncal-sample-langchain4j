0a5522fac188692fcccbb1c9c84f59a86db649df
Antonio Goncalves <antonio.goncalves@gmail.com>
2024-06-26 16:56:57 +0200

    Tools

--- ai-models-extending-models/tools/src/main/java/org/agoncal/fascicle/langchain4j/extendingprompts/tools/ChatAssistant.java
+++ ai-models-extending-models/tools/src/main/java/org/agoncal/fascicle/langchain4j/extendingprompts/tools/ChatAssistant.java
@@ -2,22 +2,29 @@ package org.agoncal.fascicle.langchain4j.extendingprompts.tools;
 
 // tag::adocHeader[]
 
+import dev.langchain4j.agent.tool.DefaultToolExecutor;
+import dev.langchain4j.agent.tool.ToolExecutionRequest;
+import dev.langchain4j.agent.tool.ToolExecutor;
 import dev.langchain4j.agent.tool.ToolSpecification;
 import dev.langchain4j.agent.tool.ToolSpecifications;
 import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.ChatMessage;
+import dev.langchain4j.data.message.ToolExecutionResultMessage;
 import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import static dev.langchain4j.model.openai.OpenAiChatModelName.GPT_4_O;
-import dev.langchain4j.model.output.Response;
 import static java.time.Duration.ofSeconds;
 import static java.util.Collections.singletonList;
+import static java.util.stream.Collectors.toList;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class ChatAssistant {
 
-  public static void main(String[] args) throws NoSuchMethodException {
+  public static void main(String[] args) throws Exception {
 
     ChatLanguageModel model = OpenAiChatModel.builder()
       .apiKey(System.getenv("OPENAI_API_KEY"))
@@ -30,41 +37,65 @@ public class ChatAssistant {
       // end::adocSkip[]
       .build();
 
-//    ToolSpecification toolSpecification = ToolSpecification.builder()
-//      .name("getWeather")
-//      .description("Returns the weather forecast for a given city")
-//      .addParameter("city", type("string"), description("The city for which the weather forecast should be returned"))
-//      .addParameter("temperatureUnit", enums(TemperatureUnit.class)) // enum TemperatureUnit { CELSIUS, FAHRENHEIT }
-//      .build();
 
-//    UserMessage question = new UserMessage("What is the capital city of France?");
-//    Response<AiMessage> response;
-
+    // STEP 1: User specify tools and query
+    System.out.println("####################################");
+    System.out.println("STEP 1: User specify tools and query");
+    // Tools
     List<ToolSpecification> toolSpecifications = ToolSpecifications.toolSpecificationsFrom(WeatherTools.class);
+    // User query
+    List<ChatMessage> chatMessages = new ArrayList<>();
     UserMessage userMessage = UserMessage.from("What will the weather be like in London tomorrow?");
-    Response<AiMessage> response = model.generate(singletonList(userMessage), toolSpecifications);
-    AiMessage aiMessage = response.content();
+    chatMessages.add(userMessage);
 
-    System.out.println(aiMessage);
+    // STEP 2: Model generate function arguments
+    System.out.println("#########################################");
+    System.out.println("STEP 2: Model generate function arguments");
+    // Tool_choice: With multiple tools it's set to "auto" by default.
+    AiMessage aiMessage = model.generate(singletonList(userMessage), toolSpecifications).content();
+    aiMessage.toolExecutionRequests().forEach(toolSpec -> { // return all tools to call to answer the user query
+      System.out.println("Function name: " + toolSpec.name());
+      System.out.println("Function args:" + toolSpec.arguments());
+    });
+    chatMessages.add(aiMessage);
 
-    // No Tools
-//    response = model.generate(List.of(question));
-//    System.out.println("============= 1");
-//    System.out.println(response.content());
+    // STEP 3: User execute function to obtain tool results
+    System.out.println("####################################################");
+    System.out.println("STEP 3: User execute function to obtain tool results");
+    WeatherTools tools = new WeatherTools();
+    ToolExecutor toolExecutor = new DefaultToolExecutor(tools, tools.getClass().getDeclaredMethod("getWeather", String.class));
+    ToolExecutionRequest toolExecutionRequest = aiMessage.toolExecutionRequests().get(0);
+    String result = toolExecutor.execute(toolExecutionRequest, UUID.randomUUID().toString());
+    ToolExecutionResultMessage toolExecutionResultMessages = ToolExecutionResultMessage.from(toolExecutionRequest, result);
+    chatMessages.addAll(List.of(toolExecutionResultMessages));
 
-    // One Tools method in a class
-//    Method lastUpdatePrivacy = TermsAndConditionTools.class.getDeclaredMethod("lastUpdatePrivacy");
-//    ToolSpecification toolSpecification = ToolSpecifications.toolSpecificationFrom(lastUpdatePrivacy);
-//    response = model.generate(List.of(question), toolSpecification);
-//    System.out.println("============= 2");
-//    System.out.println(response.content());
+    // STEP 4: Model generate final response
+    System.out.println("#####################################");
+    System.out.println("STEP 4: Model generate final response");
+    AiMessage finalResponse = model.generate(chatMessages).content();
+    System.out.println(finalResponse.text()); //According to the payment data, the payment status of transaction T1005 is Pending.
 
-    // One class with several tools
-//    List<ToolSpecification> toolSpecifications = ToolSpecifications.toolSpecificationsFrom(TermsAndConditionTools.class);
-//    response = model.generate(List.of(question), toolSpecification);
-//    System.out.println("============= 3");
-//    System.out.println(response.content());
 
   }
   // end::adocMethod[]
+
+  private static List<ToolExecutionResultMessage> toolExecutor(
+    Object objectWithTools,
+    List<ToolExecutionRequest> toolExecutionRequests) throws Exception {
+    String memoryId = UUID.randomUUID().toString();
+    return toolExecutionRequests.stream()
+      .map(request -> {
+        try {
+          ToolExecutor toolExecutor = new DefaultToolExecutor(objectWithTools,
+            objectWithTools.getClass().getDeclaredMethod(request.name(),
+              String.class));
+          return ToolExecutionResultMessage.from(request, toolExecutor.execute(request, memoryId));
+        } catch (NoSuchMethodException e) {
+          System.err.println("No such tool found: " + request.name());
+        }
+        return null;
+      })
+      .collect(toList());
+  }
+
 }
