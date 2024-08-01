13a92fb66273dd5625739944b3e60d517b1e5c39
Antonio Goncalves <antonio.goncalves@gmail.com>
2024-08-01 17:21:08 +0200

    Split

--- rag-processing-documents/splitter/src/main/java/org/agoncal/fascicle/langchain4j/processdoc/splitter/DocumentSplitterExamples.java
+++ rag-processing-documents/splitter/src/main/java/org/agoncal/fascicle/langchain4j/processdoc/splitter/DocumentSplitterExamples.java
@@ -3,9 +3,12 @@ package org.agoncal.fascicle.langchain4j.processdoc.splitter;
 import dev.langchain4j.data.document.Document;
 import dev.langchain4j.data.document.DocumentLoader;
 import dev.langchain4j.data.document.DocumentSplitter;
+import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
 import dev.langchain4j.data.document.parser.TextDocumentParser;
 import dev.langchain4j.data.document.source.FileSystemSource;
+import dev.langchain4j.data.document.splitter.DocumentByLineSplitter;
 import dev.langchain4j.data.document.splitter.DocumentBySentenceSplitter;
+import dev.langchain4j.data.document.splitter.DocumentByWordSplitter;
 import dev.langchain4j.data.segment.TextSegment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,12 +25,14 @@ public class DocumentSplitterExamples {
   private static final Logger log = LoggerFactory.getLogger(DocumentSplitterExamples.class);
 
   public static void main(String[] args) throws MalformedURLException {
-    splitWithDocumentBySentenceSplitter();
+//    splitWithDocumentBySentenceSplitter();
+//    splitWithDocumentByLineSplitter();
+    splitWithDocumentByWordSplitter();
   }
 
-  private static void splitWithDocumentBySentenceSplitter() {
+  private static void splitWithDocumentBySentenceSplitterLoad() {
     // tag::adocSplitWithDocumentBySentenceSplitter[]
-    Path documentPath = toPath("data/biography-of-isaac-asimov");
+    Path documentPath = toPath("data/biography-of-isaac-asimov.txt");
 
     Document document = DocumentLoader.load(new FileSystemSource(documentPath), new TextDocumentParser());
 
@@ -38,6 +43,52 @@ public class DocumentSplitterExamples {
     segments.forEach(segment -> System.out.println(segment.text().length()));
 
     // end::adocSplitWithDocumentBySentenceSplitter[]
+  }
+
+  private static void splitWithDocumentBySentenceSplitter() {
+    System.out.println("\n### splitWithDocumentBySentenceSplitter");
+    // tag::adocSplitWithDocumentBySentenceSplitter[]
+    Path documentPath = toPath("data/biography-of-isaac-asimov.txt");
+
+    Document document = FileSystemDocumentLoader.loadDocument(documentPath);
+
+    DocumentSplitter splitter = new DocumentBySentenceSplitter(1000, 50);
+
+    List<TextSegment> segments = splitter.split(document);
+
+    segments.forEach(segment -> System.out.println(segment.text()));
+    // end::adocSplitWithDocumentBySentenceSplitter[]
+  }
+
+  private static void splitWithDocumentByLineSplitter() {
+    System.out.println("\n### splitWithDocumentByLineSplitter");
+    Path documentPath = toPath("data/biography-of-isaac-asimov.txt");
+
+    Document document = FileSystemDocumentLoader.loadDocument(documentPath);
+
+    // tag::adocSplitWithDocumentByLineSplitter[]
+    DocumentSplitter splitter = new DocumentByLineSplitter(1000, 50);
+
+    List<TextSegment> segments = splitter.split(document);
+    // end::adocSplitWithDocumentByLineSplitter[]
+
+    segments.forEach(segment -> System.out.println(">>"+segment.text()));
+  }
+
+
+  private static void splitWithDocumentByWordSplitter() {
+    System.out.println("\n### splitWithDocumentByWordSplitter");
+    Path documentPath = toPath("data/biography-of-isaac-asimov.txt");
+
+    Document document = FileSystemDocumentLoader.loadDocument(documentPath);
+
+    // tag::adocSplitWithDocumentByWordSplitter[]
+    DocumentSplitter splitter = new DocumentByWordSplitter(1000, 50);
+
+    List<TextSegment> segments = splitter.split(document);
+    // end::adocSplitWithDocumentByWordSplitter[]
+
+    segments.forEach(segment -> System.out.println(">>"+segment.text()));
   }
 
   private static Path toPath(String fileName) {
