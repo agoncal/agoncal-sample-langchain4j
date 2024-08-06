84b8a2ce93b609161e2b192aa131f1b4a237aec2
Antonio Goncalves <antonio.goncalves@gmail.com>
2024-08-06 10:39:38 +0200

    Bge Small

--- rag-handling-embeddings/embedding-local/src/main/java/org/agoncal/fascicle/langchain4j/embedding/allmini/MusicianAssistant.java
+++ rag-handling-embeddings/embedding-local/src/main/java/org/agoncal/fascicle/langchain4j/embedding/allmini/MusicianAssistant.java
@@ -4,6 +4,7 @@ import dev.langchain4j.data.embedding.Embedding;
 import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.model.embedding.onnx.allminilml6v2.AllMiniLmL6V2EmbeddingModel;
+import dev.langchain4j.model.embedding.onnx.bgesmallen.BgeSmallEnEmbeddingModel;
 import dev.langchain4j.model.output.Response;
 
 // tag::adocSkip[]
@@ -19,7 +20,8 @@ public class MusicianAssistant {
   public static void main(String[] args) {
     MusicianAssistant musicianAssistant = new MusicianAssistant();
 
-    musicianAssistant.textToEmbeddingAllMiniLmL6V2();
+//    musicianAssistant.textToEmbeddingAllMiniLmL6V2();
+    musicianAssistant.textToEmbeddingBgeSmall();
   }
 
   public void textToEmbeddingAllMiniLmL6V2() {
@@ -33,5 +35,18 @@ public class MusicianAssistant {
 
     System.out.println(embedding.content());
     // end::adocTextToEmbeddingAllMiniLmL6V2[]
+  }
+
+  public void textToEmbeddingBgeSmall() {
+    System.out.println("### textToEmbeddingBgeSmall");
+
+    // tag::adocTextToEmbeddingBgeSmall[]
+    EmbeddingModel model = new BgeSmallEnEmbeddingModel();
+
+    TextSegment segment = TextSegment.from("Isaac Asimov is a writer and a biochemist");
+    Response<Embedding> embedding = model.embed(segment);
+
+    System.out.println(embedding.content());
+    // end::adocTextToEmbeddingBgeSmall[]
   }
 }
