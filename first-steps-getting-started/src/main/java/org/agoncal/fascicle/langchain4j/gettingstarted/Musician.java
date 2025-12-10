745c10c652ba55f5e46f6ce4cb82cfc4ce4f85fe
Antonio Goncalves <antonio.goncalves@gmail.com>
2025-12-10 14:36:13 +0100

    Remove fascicle

--- first-steps-getting-started/src/main/java/org/agoncal/fascicle/langchain4j/gettingstarted/Musician.java
+++
@@ -1,6 +0,0 @@
-package org.agoncal.fascicle.langchain4j.gettingstarted;
-
-// tag::adocSnippet[]
-public record Musician(String firstName, String lastName, String albums) {
-}
-// end::adocSnippet[]
