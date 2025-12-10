77f7bb10d0c540d06a654be6bf8d59498126aa72
Antonio Goncalves <antonio.goncalves@gmail.com>
2025-12-10 14:42:20 +0100

    Chat

---
+++ foundry-model-router/src/main/java/org/agoncal/sample/langchain4j/modelrouter/Chat.java
@@ -0,0 +1,17 @@
+package org.agoncal.sample.langchain4j.modelrouter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static java.lang.System.exit;
+
+public class Chat {
+
+  private static final Logger LOG = LoggerFactory.getLogger(Chat.class);
+
+  private static final boolean IS_LOGGING_ENABLED = true;
+
+  public static void main(String[] args) throws Exception {
+    exit(0);
+  }
+}
